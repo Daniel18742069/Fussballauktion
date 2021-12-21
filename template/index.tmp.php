bd878eb78539c529892b154df832a9dcb55d39f7
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2021-12-21 10:07:44 +0100

    Playerselection

--- template/index.tmp.php
+++ template/index.tmp.php
@@ -2,12 +2,40 @@
 <html>
 
 <body>
-	<!--Searchbar-->
-	<form action="index.php" method="get">
-		<input name="act" type="hidden" value="search" />
-		<input id="searchbar" name="name" type="text" placeholder="Spieler Suchen" />
-		<input name="submit" type="submit" />
-	</form>
+	<header>
+		<!--Searchbar-->
+		<form action="index.php" method="get">
+			<input name="act" type="hidden" value="search" />
+			<input id="searchbar" name="name" type="text" placeholder="Spieler Suchen" />
+			<input name="submit" type="submit" />
+		</form>
+	</header>
+
+	<main>
+		<h1>Spieler:</h1>
+		<table>
+			<thead>
+				<th>Name</th>
+				<th>Position</th>
+				<th>Team</th>
+			</thead>
+			<?php foreach (CONTENT['Players'] as $Player) : ?>
+				<tr>
+					<td>
+						<a href="index.php?act=player&index=<?= $Player['index']; ?>">
+							<?= $Player['name']; ?>
+						</a>
+					</td>
+					<td>
+						<?= $Player['position']; ?>
+					</td>
+					<td>
+						<?= $Player['team']; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+	</main>
 </body>
 
 </html>
