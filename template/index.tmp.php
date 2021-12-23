532cdf744f2e9151a1d02f1ec479e11eaecd5254
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2021-12-23 14:56:42 +0100

    Datei Neuordnung

--- template/index.tmp.php
+++ template/index.tmp.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/index.css" media="screen" />
+  <title>Sign in</title>
+</head>
+
 <body>
 	<header>
 		<!--Website Logo-->
--- template/login.tmp.php
+++ template/login.tmp.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/login.css" media="screen" />
+	<title>Sign in</title>
+</head>
+
 <body>
 	<header>
 		<!--Website Logo-->
@@ -10,9 +19,16 @@
 	</header>
 
 	<main>
-		<h2>Login:</h2>
 		<!--Login-->
-		<?php include 'Include/login.html'; ?>
+		<div class="main">
+			<p class="sign" align="center">Login</p>
+			<form class="form1" action="index.php" method="post">
+				<input name="act" type="hidden" value="login" />
+				<input class="un" name="name" type="text" align="center" placeholder="Benutzername" />
+				<input class="pass" name="pass" type="password" align="center" placeholder="Passwort" />
+				<input name="submit" type="submit" class="submit" align="center" value="Anmelden" />
+			</form>
+		</div>
 	</main>
 </body>
 
