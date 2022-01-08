588380eec24f1e53f3f80f0bc443470e021477de
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-01-08 15:40:35 +0100

    pfadänderung2

--- template/index.tmp.php
+++ template/index.tmp.php
@@ -13,7 +13,7 @@
 <body>
 	<header>
 		<!--Website Logo-->
-		<a href="index.php" class="logo"><img src="../Bilder/header.png" alt="Football Maniacs!"></a>
+		<a href="index.php" class="logo"><img src="Bilder/header.png" alt="Football Maniacs!"></a>
 
 		<!--Searchbar-->
 		<?php include 'Include/searchbar.html'; ?>
