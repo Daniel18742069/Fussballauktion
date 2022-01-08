8cd1b11c68d2f1fe86bb18b336f49d05241d3ff0
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-01-08 15:33:30 +0100

    suche mit css verknüpft

--- template/search.tmp.php
+++ template/search.tmp.php
@@ -4,13 +4,14 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
-	<link rel="stylesheet" href="css/search.css" media="screen" />
+	<link rel="stylesheet" href="css/index.css" media="screen" />
 	<title>Searchbar</title>
 </head>
 
 <body>
 	<header>
 		<!--Website Logo-->
+		<img src="Bilder/header.png">
 		<h1>
 			<a href="index.php">Football Maniacs!</a>
 		</h1>
@@ -31,10 +32,11 @@
 		<?php endif; ?>
 	</header>
 
+
+
+
 	<main>
-		<?php
-		?>
-		<!--Errormessage-->
+	<!--Errormessage-->
 		<?php if (isset(CONTENT['Error'])) : ?>
 
 			<p id="error_message">
@@ -42,43 +44,6 @@
 			</p>
 
 		<?php else : ?>
-
-			<h2>Spieler:</h2>
-			<table>
-
-				<thead>
-					<th>Name</th>
-					<th>Position</th>
-					<th>Team</th>
-				</thead>
-
-				<!--Search Output-->
-				<?php foreach (CONTENT['Players'] as $Player) : ?>
-
-					<tr>
-						<td>
-							<a href="index.php?act=player&index=<?= $Player['index']; ?>">
-								<?= $Player['name']; ?>
-							</a>
-						</td>
-						<td>
-							<?= $Player['position']; ?>
-						</td>
-						<td>
-							<?= $Player['team']; ?>
-						</td>
-					</tr>
-
-				<?php endforeach; ?>
-			</table>
-
-		<?php endif; ?>
-	</main>
-
-
-
-
-	<main>
 		<table>
 
 			<div class="container">
@@ -113,7 +78,7 @@
 			</div>
 
 		</table>
-
+		<?php endif; ?>
 
 	</main>
 	
