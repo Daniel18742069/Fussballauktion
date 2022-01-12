7ad51d7b9cc4db94e32fd98050a8bf1eaf4aa882
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2022-01-12 22:50:56 +0100

    hotfix
    moved config file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'config.php';
+require_once 'Config/config.php';
 require_once 'Controller/controller.php';
 require_once 'Model/database.php';
 require_once 'Model/player.php';
