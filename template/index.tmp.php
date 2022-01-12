024a5a703d95e4b5915f1025a56d27e092574353
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2022-01-12 22:58:34 +0100

    changed folder name

--- template/index.tmp.php
+++ template/index.tmp.php
@@ -4,7 +4,7 @@
 <head>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/index.css" media="screen" />
+	<link rel="stylesheet" href="CSS/index.css" media="screen" />
 	<title>Fussballauktion</title>
 </head>
 
